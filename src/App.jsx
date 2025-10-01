043975bdd9cb7f39c18dfd3d855a6878e54f012f
BewleyRichard <118364533+BewleyRichard@users.noreply.github.com>
2025-10-01 17:10:43 +0100

    MainDetails component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,15 @@
 import React from "react";
 import './App.css';
 import LeftSidebar from "./components/LeftSidebar.jsx";
+import MainDetails from "./components/MainDetails.jsx";
 
 function App() {
   return (
     <div className="App">
       <LeftSidebar />
+      <MainDetails />
 
-      <div className="main-details">
-        <h1>X</h1>
-        {/* TODO: add an images here */}
-        <p>My name is Richard and I express myself digitally in order to experiment, have fun (this is the most important thing), and to understand this world in order to create new ones.</p>
-
-        <h3>Experience:</h3>
-        <ul className="experience-list">
-          <li>2025 Ada Mode</li>
-          <li>2025 Windscope</li>
-          <li>2024 Ada Mode</li>
-        </ul>
-
-        <h3>Tools:</h3>
-        <ul className="small-list">
-          <li>Figma</li>
-          <li>FigJam</li>
-          <li>Miro</li>
-        </ul>
-      </div>
+      
 
       <div className="hero-image-section">
         <h1>X</h1>
---
+++ src/components/MainDetails.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function MainDetails() {
+  return (
+    <div className="main-details">
+        <h1>X</h1>
+        {/* TODO: add an images here */}
+        <p>My name is Richard and I express myself digitally in order to experiment, have fun (this is the most important thing), and to understand this world in order to create new ones.</p>
+
+        <h3>Experience:</h3>
+        <ul className="experience-list">
+          <li>2025 Ada Mode</li>
+          <li>2025 Windscope</li>
+          <li>2024 Ada Mode</li>
+        </ul>
+
+        <h3>Tools:</h3>
+        <ul className="small-list">
+          <li>Figma</li>
+          <li>FigJam</li>
+          <li>Miro</li>
+        </ul>
+      </div>
+  );
+}
+
+export default MainDetails;
