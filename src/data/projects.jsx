934d04b3a1881ce95d103e98d7e563accc5dc678
BewleyRichard <118364533+BewleyRichard@users.noreply.github.com>
2025-10-02 13:30:31 +0100

    Template local database added to src/data/projects.

---
+++ src/data/projects.jsx
@@ -0,0 +1,32 @@
+const projects = [
+    {
+      id: 1,
+      title: "Autonomous Material Handling UI",
+      year: 2025,
+      client: "Ada Mode",
+      summary: "A system to optimize material handling workflows with a digital interface.",
+      deliverables: ["Journey Maps", "Competitive Analysis", "User Flows", "A/B Testing", "Developer Handoff", "Wireframes", "Heuristic Evaluation"],
+      tools: ["Figma", "FigJam", "Miro"]
+    },
+    {
+      id: 2,
+      title: "Implementing Granular Alert Subscriptions",
+      year: 2025,
+      client: "Windscope",
+      summary: "Designing a system to allow users to customise alert preferences in a granular way.",
+      deliverables: ["User Flows", "Prototypes", "Wireframes"],
+      tools: ["Figma", "Miro"]
+    },
+    {
+      id: 3,
+      title: "Creating a referencing system",
+      year: 2025,
+      client: "Ada Mode",
+      summary: "Creating a referencing system for an LLM Doc managment system",
+      deliverables: ["User Flows", "Prototypes", "Wireframes"],
+      tools: ["Figma", "Miro"]
+    }
+  ];
+  
+  export default projects;
+  
