a5864740201418a94c7faed09358c862fe43b115
BewleyRichard <118364533+BewleyRichard@users.noreply.github.com>
2025-10-02 11:29:06 +0100

    Folders created for each components css. Keep global css in App.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// src/App.jsx
 import React from "react";
 import './App.css';
-import LeftSidebar from "./components/LeftSidebar.jsx";
-import MainDetails from "./components/MainDetails.jsx";
-import HeroImageSection from "./components/HeroImageSection.jsx";
-import SingleProject from "./components/SingleProject.jsx";
-import SideGallery from "./components/SideGallery.jsx";
+import LeftSidebar from "./components/LeftSidebar/LeftSidebar.jsx";
+import MainDetails from "./components/MainDetails/MainDetails.jsx";
+import HeroImageSection from "./components/HeroImageSection/HeroImageSection.jsx";
+import SingleProject from "./components/SingleProject/SingleProject.jsx";
+import SideGallery from "./components/SideGallery/SideGallery.jsx";
 
 function App() {
   return (
--- src/components/HeroImageSection/HeroImageSection.jsx
+++ src/components/HeroImageSection/HeroImageSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./HeroImageSection.css";
+
 
 function HeroImageSection() {
   return (
